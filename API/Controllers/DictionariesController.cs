6ef1c71a706bd979f7874a22642810bc1f39265a
Vadym Ohyr <dusty3ntity@gmail.com>
2020-03-17 18:22:05 +0200

    feat(dictionaries): Add edit handler

--- API/Controllers/DictionariesController.cs
+++ API/Controllers/DictionariesController.cs
@@ -27,6 +27,13 @@ namespace API.Controllers
             return await Mediator.Send(command);
         }
 
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await Mediator.Send(command);
+        }
+
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
