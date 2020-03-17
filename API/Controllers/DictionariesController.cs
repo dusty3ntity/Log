8a78db53a1c1dcc12f8c66311fe54e64bbc0d061
Vadym Ohyr <dusty3ntity@gmail.com>
2020-03-17 12:52:18 +0200

    feat(dictionaries): Add Dictionaries list handler

--- API/Controllers/DictionariesController.cs
+++ API/Controllers/DictionariesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Dictionaries;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,12 @@ namespace API.Controllers
 {
     public class DictionariesController : BaseController
     {
+        [HttpGet]
+        public async Task<ActionResult<List<DictionaryDto>>> List()
+        {
+            return await Mediator.Send(new List.Query());
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<DictionaryDto>> Details(Guid id)
         {
