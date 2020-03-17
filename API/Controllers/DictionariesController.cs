4c5c1aafa4c57e77e84f5aee84291e011dc827e7
Vadym Ohyr <dusty3ntity@gmail.com>
2020-03-17 12:36:58 +0200

    feat(dictionary): Add Dictionaries controller, details handler

---
+++ API/Controllers/DictionariesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Dictionaries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class DictionariesController : BaseController
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DictionaryDto>> Details(Guid id)
+        {
+            return await Mediator.Send(new Details.Query {Id = id});
+        }
+    }
+}
---
+++ Application/Dictionaries/DictionaryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain;
+
+namespace Application.Dictionaries
+{
+    public class DictionaryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Language KnownLanguage { get; set; }
+        public Language LanguageToLearn { get; set; }
+
+        public int WordsCount { get; set; }
+        public int PhrasesCount { get; set; }
+        public int LearnedItemsCount { get; set; }
+        public int PreferredLearningListSize { get; set; }
+    }
+}
