80af7dba86431956cd684ed5383fc5f28c199d01
Vadym Ohyr <dusty3ntity@gmail.com>
2020-03-27 13:11:50 +0200

    feat(exceptions): Add custom exceptions to throw correct http response codes

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Application.Dictionaries;
 using Application.Interfaces;
 using Application.LearningLists;
@@ -45,9 +46,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
---
+++ Application/Errors/RestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace Application.Errors
+{
+    public class RestException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+        public object Errors { get; }
+
+        public RestException(HttpStatusCode statusCode, object errors = null)
+        {
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+    }
+}
