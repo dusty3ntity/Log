2e0312719500dab205ece0998591303170f1bb5c
Vadym Ohyr <dusty3ntity@gmail.com>
2020-05-07 15:51:42 +0300

    feat(client): Update filters checkboxes (closes #15)

--- client-app/src/components/dashboard/ItemFilters.tsx
+++ client-app/src/components/dashboard/ItemFilters.tsx
@@ -8,24 +8,24 @@ const ItemFilters = () => {
 			<div id="filters-container">
 				<div id="filters-category">
 					<h3>Category</h3>
-					<Checkbox>Words</Checkbox>
+					<Checkbox className="checkbox">Words</Checkbox>
 					<br />
-					<Checkbox>Phrases</Checkbox>
+					<Checkbox className="checkbox">Phrases</Checkbox>
 				</div>
 
 				<Divider />
 
 				<div id="filters-progress">
 					<h3>Progress</h3>
-					<Checkbox>
+					<Checkbox className="checkbox">
 						Learned <Badge status="success" />
 					</Checkbox>
 					<br />
-					<Checkbox>
+					<Checkbox className="checkbox">
 						In progress <Badge status="warning" />
 					</Checkbox>
 					<br />
-					<Checkbox>
+					<Checkbox className="checkbox">
 						Untouched <Badge status="default" />
 					</Checkbox>
 				</div>
