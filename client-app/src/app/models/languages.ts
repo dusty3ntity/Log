bdaf1f93c2e99566e284bef962ed695b5edf4aba
Vadym Ohyr <dusty3ntity@gmail.com>
2020-07-22 23:35:45 +0300

    feat(client): Add languages (closes #166)

--- client-app/src/app/models/languages.ts
+++ client-app/src/app/models/languages.ts
@@ -1,20 +1,545 @@
-import { ILanguage } from './dictionary';
+import { ILanguage } from "./dictionary";
 
 export const languagesList: ILanguage[] = [
 	{
 		id: 1,
+		name: "Afrikaans",
+		isoCode: "afr",
+	},
+	{
+		id: 2,
+		name: "Albanian",
+		isoCode: "sqi",
+	},
+	{
+		id: 3,
+		name: "Amharic",
+		isoCode: "amh",
+	},
+	{
+		id: 4,
+		name: "Arabic",
+		isoCode: "ara",
+	},
+	{
+		id: 5,
+		name: "Armenian",
+		isoCode: "hye",
+	},
+	{
+		id: 6,
+		name: "Azerbaijani",
+		isoCode: "aze",
+	},
+	{
+		id: 7,
+		name: "Basque",
+		isoCode: "eus",
+	},
+	{
+		id: 8,
+		name: "Belarusian",
+		isoCode: "bel",
+	},
+	{
+		id: 9,
+		name: "Bengali",
+		isoCode: "ben",
+	},
+	{
+		id: 10,
+		name: "Bosnian",
+		isoCode: "bos",
+	},
+	{
+		id: 11,
+		name: "Bulgarian",
+		isoCode: "bul",
+	},
+	{
+		id: 12,
+		name: "Catalan",
+		isoCode: "cat",
+	},
+	{
+		id: 13,
+		name: "Cebuano",
+		isoCode: "ceb",
+	},
+	{
+		id: 14,
+		name: "Chichewa",
+		isoCode: "nya",
+	},
+	{
+		id: 15,
+		name: "Chinese",
+		isoCode: "zho",
+	},
+	{
+		id: 16,
+		name: "Corsican",
+		isoCode: "cos",
+	},
+	{
+		id: 17,
+		name: "Croatian",
+		isoCode: "hrv",
+	},
+	{
+		id: 18,
+		name: "Czech",
+		isoCode: "ces",
+	},
+	{
+		id: 19,
+		name: "Danish",
+		isoCode: "dan",
+	},
+	{
+		id: 20,
+		name: "Dutch",
+		isoCode: "nld",
+	},
+	{
+		id: 21,
 		name: "English",
 		isoCode: "eng",
 	},
 	{
-		id: 2,
-		name: "Русский",
+		id: 22,
+		name: "Esperanto",
+		isoCode: "epo",
+	},
+	{
+		id: 23,
+		name: "Estonian",
+		isoCode: "est",
+	},
+	{
+		id: 24,
+		name: "Filipino",
+		isoCode: "fil",
+	},
+	{
+		id: 25,
+		name: "Finnish",
+		isoCode: "fin",
+	},
+	{
+		id: 26,
+		name: "French",
+		isoCode: "fra",
+	},
+	{
+		id: 27,
+		name: "Frisian",
+		isoCode: "frr",
+	},
+	{
+		id: 28,
+		name: "Galician",
+		isoCode: "glg",
+	},
+	{
+		id: 29,
+		name: "Georgian",
+		isoCode: "kat",
+	},
+	{
+		id: 30,
+		name: "German",
+		isoCode: "deu",
+	},
+	{
+		id: 31,
+		name: "Greek",
+		isoCode: "ell",
+	},
+	{
+		id: 32,
+		name: "Gujarati",
+		isoCode: "guj",
+	},
+	{
+		id: 33,
+		name: "Haitian Creole",
+		isoCode: "hat",
+	},
+	{
+		id: 34,
+		name: "Hausa",
+		isoCode: "hau",
+	},
+	{
+		id: 35,
+		name: "Hawaiian",
+		isoCode: "haw",
+	},
+	{
+		id: 36,
+		name: "Hebrew",
+		isoCode: "heb",
+	},
+	{
+		id: 37,
+		name: "Hindi",
+		isoCode: "hin",
+	},
+	{
+		id: 38,
+		name: "Hmong",
+		isoCode: "hmn",
+	},
+	{
+		id: 39,
+		name: "Hungarian",
+		isoCode: "hun",
+	},
+	{
+		id: 40,
+		name: "Icelandic",
+		isoCode: "isl",
+	},
+	{
+		id: 41,
+		name: "Igbo",
+		isoCode: "ibo",
+	},
+	{
+		id: 42,
+		name: "Indonesian",
+		isoCode: "ind",
+	},
+	{
+		id: 43,
+		name: "Irish",
+		isoCode: "gle",
+	},
+	{
+		id: 44,
+		name: "Italian",
+		isoCode: "ita",
+	},
+	{
+		id: 45,
+		name: "Japanese",
+		isoCode: "jpn",
+	},
+	{
+		id: 46,
+		name: "Javanese",
+		isoCode: "jav",
+	},
+	{
+		id: 47,
+		name: "Kannada",
+		isoCode: "kan",
+	},
+	{
+		id: 48,
+		name: "Kazakh",
+		isoCode: "kaz",
+	},
+	{
+		id: 49,
+		name: "Khmer",
+		isoCode: "khm",
+	},
+	{
+		id: 50,
+		name: "Kinyarwanda",
+		isoCode: "kin",
+	},
+	{
+		id: 51,
+		name: "Korean",
+		isoCode: "kor",
+	},
+	{
+		id: 52,
+		name: "Kurdish",
+		isoCode: "kur",
+	},
+	{
+		id: 53,
+		name: "Kyrgyz",
+		isoCode: "kir",
+	},
+	{
+		id: 54,
+		name: "Lao",
+		isoCode: "lao",
+	},
+	{
+		id: 55,
+		name: "Latin",
+		isoCode: "lat",
+	},
+	{
+		id: 56,
+		name: "Latvian",
+		isoCode: "lav",
+	},
+	{
+		id: 57,
+		name: "Lithuanian",
+		isoCode: "lit",
+	},
+	{
+		id: 58,
+		name: "Luxembourgish",
+		isoCode: "ltz",
+	},
+	{
+		id: 59,
+		name: "Macedonian",
+		isoCode: "mkd",
+	},
+	{
+		id: 60,
+		name: "Malagasy",
+		isoCode: "mlg",
+	},
+	{
+		id: 61,
+		name: "Malay",
+		isoCode: "msa",
+	},
+	{
+		id: 62,
+		name: "Malayalam",
+		isoCode: "mal",
+	},
+	{
+		id: 63,
+		name: "Maltese",
+		isoCode: "mlt",
+	},
+	{
+		id: 64,
+		name: "Maori",
+		isoCode: "mri",
+	},
+	{
+		id: 65,
+		name: "Marathi",
+		isoCode: "mar",
+	},
+	{
+		id: 66,
+		name: "Mongolian",
+		isoCode: "mon",
+	},
+	{
+		id: 67,
+		name: "Myanmar",
+		isoCode: "mya",
+	},
+	{
+		id: 68,
+		name: "Nepali",
+		isoCode: "nep",
+	},
+	{
+		id: 69,
+		name: "Norwegian",
+		isoCode: "nor",
+	},
+	{
+		id: 70,
+		name: "Odia",
+		isoCode: "ori",
+	},
+	{
+		id: 71,
+		name: "Pashto",
+		isoCode: "pus",
+	},
+	{
+		id: 72,
+		name: "Persian",
+		isoCode: "fas",
+	},
+	{
+		id: 73,
+		name: "Polish",
+		isoCode: "pol",
+	},
+	{
+		id: 74,
+		name: "Portuguese",
+		isoCode: "por",
+	},
+	{
+		id: 75,
+		name: "Punjabi",
+		isoCode: "pan",
+	},
+	{
+		id: 76,
+		name: "Romanian",
+		isoCode: "ron",
+	},
+	{
+		id: 77,
+		name: "Russian",
 		isoCode: "rus",
 	},
 	{
-		id: 3,
-		name: "Українська",
+		id: 78,
+		name: "Samoan",
+		isoCode: "smo",
+	},
+	{
+		id: 79,
+		name: "Scots Gaelic",
+		isoCode: "gla",
+	},
+	{
+		id: 80,
+		name: "Serbian",
+		isoCode: "srp",
+	},
+	{
+		id: 81,
+		name: "Sesotho",
+		isoCode: "sot",
+	},
+	{
+		id: 82,
+		name: "Shona",
+		isoCode: "sna",
+	},
+	{
+		id: 83,
+		name: "Sindhi",
+		isoCode: "snd",
+	},
+	{
+		id: 84,
+		name: "Sinhala",
+		isoCode: "sin",
+	},
+	{
+		id: 85,
+		name: "Slovak",
+		isoCode: "slk",
+	},
+	{
+		id: 86,
+		name: "Slovenian",
+		isoCode: "slv",
+	},
+	{
+		id: 87,
+		name: "Somali",
+		isoCode: "som",
+	},
+	{
+		id: 88,
+		name: "Spanish",
+		isoCode: "spa",
+	},
+	{
+		id: 89,
+		name: "Sundanese",
+		isoCode: "sun",
+	},
+	{
+		id: 90,
+		name: "Swahili",
+		isoCode: "swa",
+	},
+	{
+		id: 91,
+		name: "Swedish",
+		isoCode: "swe",
+	},
+	{
+		id: 92,
+		name: "Tajik",
+		isoCode: "tgk",
+	},
+	{
+		id: 93,
+		name: "Tamil",
+		isoCode: "tam",
+	},
+	{
+		id: 94,
+		name: "Tatar",
+		isoCode: "tat",
+	},
+	{
+		id: 95,
+		name: "Telugu",
+		isoCode: "tel",
+	},
+	{
+		id: 96,
+		name: "Thai",
+		isoCode: "tha",
+	},
+	{
+		id: 97,
+		name: "Turkish",
+		isoCode: "tur",
+	},
+	{
+		id: 98,
+		name: "Turkmen",
+		isoCode: "tuk",
+	},
+	{
+		id: 99,
+		name: "Ukrainian",
 		isoCode: "ukr",
+	},
+	{
+		id: 100,
+		name: "Urdu",
+		isoCode: "urd",
+	},
+	{
+		id: 101,
+		name: "Uyghur",
+		isoCode: "uig",
+	},
+	{
+		id: 102,
+		name: "Uzbek",
+		isoCode: "uzb",
+	},
+	{
+		id: 103,
+		name: "Vietnamese",
+		isoCode: "vie",
+	},
+	{
+		id: 104,
+		name: "Welsh",
+		isoCode: "cym",
+	},
+	{
+		id: 105,
+		name: "Xhosa",
+		isoCode: "xho",
+	},
+	{
+		id: 106,
+		name: "Yiddish",
+		isoCode: "yid",
+	},
+	{
+		id: 107,
+		name: "Yoruba",
+		isoCode: "yor",
+	},
+	{
+		id: 108,
+		name: "Zulu",
+		isoCode: "zul",
 	},
 ];
 
