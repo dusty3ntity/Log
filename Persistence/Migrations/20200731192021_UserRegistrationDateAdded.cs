55a70086103f52580012a953d6820cb7504bb9fa
Vadym Ohyr <dusty3ntity@gmail.com>
2020-07-31 23:07:31 +0300

    feat(api): Add registration date field to the AppUser entity

---
+++ Persistence/Migrations/20200731192021_UserRegistrationDateAdded.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class UserRegistrationDateAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationDate",
+                table: "AspNetUsers");
+        }
+    }
+}
